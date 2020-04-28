91a16ca41584c16ed5671bfd46b28108f662b9fa
Lucas de Melo Lima <luucasmell@gmail.com>
2020-04-28 17:19:25 -0300

    Create method to set the play

--- app/src/main/java/com/example/jogodavelha/MainActivity.java
+++ app/src/main/java/com/example/jogodavelha/MainActivity.java
@@ -4,9 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final String QUADRADO = "quadrado";
+    private final String BOLINHA = "0";
+    private final String X = "X";
+    private String jogadorAtual = X;
     private View view;
 
     @Override
@@ -16,11 +21,34 @@ public class MainActivity extends AppCompatActivity {
         setContentView(getView());
     }
 
+    public Button getQuadrado(int tagNum){
+        return (Button)getView().findViewWithTag(QUADRADO + tagNum);
+    }
+
+    public void jogadaQuadrado (View v){
+        if(getJogadorAtual() == X){
+            setJogadorAtual(BOLINHA);
+            ((Button)v).setText(BOLINHA);
+        } else{
+            setJogadorAtual(X);
+            ((Button)v).setText(X);
+        }
+
+    }
+
     public View getView() {
         return view;
     }
 
     public void setView(View view) {
         this.view = view;
+    }
+
+    public String getJogadorAtual() {
+        return jogadorAtual;
+    }
+
+    public void setJogadorAtual(String jogadorAtual) {
+        this.jogadorAtual = jogadorAtual;
     }
 }
