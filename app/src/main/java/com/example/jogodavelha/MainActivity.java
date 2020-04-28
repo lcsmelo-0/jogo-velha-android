113a3e417d7c6c8545b974be2fab65ec1148fbff
Lucas de Melo Lima <luucasmell@gmail.com>
2020-04-28 19:18:16 -0300

    Create method to verify final

--- app/src/main/java/com/example/jogodavelha/MainActivity.java
+++ app/src/main/java/com/example/jogodavelha/MainActivity.java
@@ -14,6 +14,18 @@ public class MainActivity extends AppCompatActivity {
     private final String X = "X";
     private String jogadorAtual = X;
     private View view;
+    int[][] estadoFinal = new int[][]{
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9},
+
+            {1, 4, 7},
+            {2, 5, 8},
+            {3, 6, 9},
+
+            {1, 5, 9},
+            {3, 5, 7},
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +38,29 @@ public class MainActivity extends AppCompatActivity {
         return (Button)getView().findViewWithTag(QUADRADO + tagNum);
     }
 
+    public void habilitarQuadrado(boolean bool){
+        for(int i = 1; i <=9; i++){
+            if(getQuadrado(i) != null){
+                getQuadrado(i).setEnabled(bool);
+            }
+        }
+    }
+
+    public void verificarFinal(){
+        for(int x =0; x<=7; ++x){
+            String s1 = getQuadrado(estadoFinal[x][0]).getText().toString();
+            String s2 = getQuadrado(estadoFinal[x][1]).getText().toString();
+            String s3 = getQuadrado(estadoFinal[x][2]).getText().toString();
+
+            if(!s1.equals("") && !s2.equals("") && !s3.equals("")){
+                if(s1.equals(s2) && (s2.equals(s3))){
+                    Toast.makeText(getView().getContext(), "Venceu o jogador " + this.jogadorAtual, Toast.LENGTH_LONG).show();
+                    this.habilitarQuadrado(false);
+                }
+            }
+        }
+    }
+
     public void jogadaQuadrado (View v){
         if(((Button)v).getText().equals("")){
             if(getJogadorAtual() == X){
@@ -35,6 +70,7 @@ public class MainActivity extends AppCompatActivity {
                 setJogadorAtual(X);
                 ((Button)v).setText(X);
             }
+            this.verificarFinal();
         } else{
             Toast.makeText(getView().getContext(), "Escolha outro quadrado", Toast.LENGTH_LONG).show();
         }
@@ -47,10 +83,10 @@ public class MainActivity extends AppCompatActivity {
         for(int i = 1; i<= 9; i++){
             if(getQuadrado(i) != null){
                 getQuadrado(i).setText("");
-                getQuadrado(i).setEnabled(true);
-            }
+                habilitarQuadrado(true);            }
         }
     }
+
 
     public View getView() {
         return view;
